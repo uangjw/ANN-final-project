8609a5b3eb5b9ae2e6ed565bf2249a8f10d6dad2
uangjw <455950432@qq.com>
2022-04-20 17:01:56 +0800

    -

---
+++ transferLearning1.py
@@ -0,0 +1,36 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+cudnn.benchmark = True
+plt.ion()
+
+data_transforms = {
+    'train' : transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+        [0.229, 0.224, 0.225])
+    ]),
+    'val' : transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+        [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 
